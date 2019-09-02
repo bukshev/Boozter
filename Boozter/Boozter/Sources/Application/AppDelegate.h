9dd900620d9aa33f70ce2a1ad3191852b824f9e2
Ivan Numbaone <bukshev@gmail.com>
2019-09-02 20:25:33 +0700

    Init commit with empty project

---
+++ Boozter/Boozter/Sources/Application/AppDelegate.h
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.h
+//  Boozter
+//
+//  Created by Ivan Bukshev on 02/09/2019.
+//  Copyright © 2019 Team Absurdum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong) NSPersistentContainer *persistentContainer;
+
+- (void)saveContext;
+
+
+@end
+
---
+++ Boozter/Boozter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Boozter
+//
+//  Created by Ivan Bukshev on 02/09/2019.
+//  Copyright © 2019 Team Absurdum. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
